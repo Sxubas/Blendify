f95bd0fee7c9b16fe94c3776cbee89011583616a
Julián Manrique <ja.manrique@uniandes.edu.co>
2018-12-05 00:11:33 -0500

    Custom mood search basic structure

--- imports/ui/Blend/AddTracks/CustomMood/CustomMood.jsx
+++ imports/ui/Blend/AddTracks/CustomMood/CustomMood.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 
+import MoodParameter from './MoodParameter/MoodParameter.jsx';
+
 import './CustomMood.css';
 import './CustomMood.mobile.css';
 
@@ -18,7 +20,12 @@ class CustomMood extends React.Component {
   render() {
     return (
       <div className='custom-mood-container'>
-        <h5>CustomMood</h5>
+        <h3>CustomMood</h3>
+        <hr/>
+        <p>Choose which parameters do you want to query songs for</p>
+        <MoodParameter enabled title="Energy" description="Speed and loudness of the tracks (0 quiet, 1 energetic)"/>
+        <MoodParameter enabled title="Acousticness" description="How acoustic the tracks should be (0 not acoustic, 1 acoustic)"/>
+        <MoodParameter enabled title="Instrumentalness" description="The amount of vocal content in the tracks (0 vocal, 1 instrumental)"/>
       </div>
     );
   }
